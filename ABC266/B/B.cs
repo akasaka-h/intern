20a889993af08fd9dd00719e04c906b4f481b257
akasaka <akasaka@ist.hokudai.ac.jp>
2022-09-09 17:37:12 +0900

    0909

---
+++ ABC266/B/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC266
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            Int64 N = Int64.Parse(Console.ReadLine());
+            Int64 k = 998244353;
+
+            Int64 mod = N % k;
+
+            if (mod < 0)
+            {
+                mod += k;
+            }
+
+            Console.WriteLine(mod);
+        }
+    }
+}
---
+++ ABC266/C/C.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC266
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] c = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] d = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] ad = { d[0] - a[0], d[1] - a[1] };
+            int[] ba = { a[0] - b[0], a[1] - b[1] };
+            int[] cb = { b[0] - c[0], b[1] - c[1] };
+            int[] dc = { c[0] - d[0], c[1] - d[1] };
+
+            float l1 = (ad[0]*(b[1]-a[1])) - (ad[1]*(b[0]-a[0]));
+            float l2 = (ba[0]*(c[1]-b[1])) - (ba[1]*(c[0]-b[0]));
+            float l3 = (cb[0]*(d[1]-c[1])) - (cb[1]*(d[0]-c[0]));
+            float l4 = (dc[0]*(a[1]-d[1])) - (dc[1]*(a[0]-d[0]));
+
+            if ((l1 < 0) && (l2 < 0) && (l3 < 0) && (l4 < 0))
+            {
+                Console.WriteLine("Yes");
+            }else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
