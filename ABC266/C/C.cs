1f186bec35119ce529102e432412e1af8d46e209
akasaka <akasaka@ist.hokudai.ac.jp>
2022-09-13 16:40:37 +0900

    0913

--- ABC266/C/C.cs
+++ ABC266/C/C.cs
@@ -5,6 +5,17 @@ namespace ABC266
 {
     class C
     {
+        int[] line(int[]a, int[]b)
+        {
+            int[] result = {b[0] - a[0], b[1] - a[1]};
+            return result;
+        }
+
+        float outer(int[]liner, int[]a, int[]b)
+        {
+            float result = (liner[0]*(a[1]-b[1])) - (liner[1]*(a[0]-b[0]));
+            return result;
+        }
         static void Main(string[] args)
         {
 
@@ -13,15 +24,16 @@ namespace ABC266
             int[] c = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int[] d = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            int[] ad = { d[0] - a[0], d[1] - a[1] };
-            int[] ba = { a[0] - b[0], a[1] - b[1] };
-            int[] cb = { b[0] - c[0], b[1] - c[1] };
-            int[] dc = { c[0] - d[0], c[1] - d[1] };
+            var C = new C();
+            int[] ad = C.line(a,d);
+            int[] ba = C.line(b,a);
+            int[] cb = C.line(c,b);
+            int[] dc = C.line(d,c);
 
-            float l1 = (ad[0]*(b[1]-a[1])) - (ad[1]*(b[0]-a[0]));
-            float l2 = (ba[0]*(c[1]-b[1])) - (ba[1]*(c[0]-b[0]));
-            float l3 = (cb[0]*(d[1]-c[1])) - (cb[1]*(d[0]-c[0]));
-            float l4 = (dc[0]*(a[1]-d[1])) - (dc[1]*(a[0]-d[0]));
+            float l1 = C.outer(ad, b, a);
+            float l2 = C.outer(ba, c, b);
+            float l3 = C.outer(cb, d, c);
+            float l4 = C.outer(dc, a, d);
 
             if ((l1 < 0) && (l2 < 0) && (l3 < 0) && (l4 < 0))
             {
