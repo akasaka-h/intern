b0fd06f5c18c1f2d64fcbcab2c6361e9b2ce5e95
akasaka <akasaka@ist.hokudai.ac.jp>
2022-09-29 17:00:54 +0900

    0929

---
+++ ABC263/B/B.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC263
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            var B = new B();
+            var input = B.Read_int();
+            if (input == null) return ;
+            var n = input[0];
+            var P = B.Read_int();
+            int count =0;
+
+            P.Add(n);
+            var cou = P.Reverse<int>().Where(x=>x !=1).Select(x=>P[x-2]).Count();
+            Console.WriteLine(cou);
+        }
+        List<int> Read_int()
+        {
+            var check = new List<bool>();
+            var input = Console.ReadLine().Split(' ').Select(x => {
+                var temp = 0;
+                check.Add(int.TryParse(x, out temp));
+                return temp;
+            }).ToList();
+            if (check.Any(x => x == false)) return null;
+            return input;
+        }
+    }
+}
---
+++ ABC263/C/C.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC263
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            var C = new C();
+
+            var input= C.Read_int();
+            if (input == null) return ;
+            var n = input[0];
+            var k= input[1];
+
+            var line = Enumerable.Range(1,k).ToArray();
+
+            var com = C.Enumerate(line,n);
+            foreach (var item in com)
+            {
+                string s= string.Join(",", item.Select(x=>x.ToString()).ToArray());
+                Console.WriteLine(s);
+            }
+        }
+
+        IEnumerable<T[]> Enumerate<T>(IEnumerable<T> items, int n)
+        {
+            if (n == 1)
+            {
+              foreach (var item in items)
+                  yield return new T[] { item };
+              yield break;
+            }
+            foreach (var item in items)
+            {
+                var leftside = new T[] { item };
+                var unused = items.SkipWhile(e => !e.Equals(item)).Skip(1).ToList();
+
+                foreach (var rightside in Enumerate(unused, n - 1))
+                {
+                  yield return leftside.Concat(rightside).ToArray();
+                }
+            }
+        }
+
+        //入力
+        List<int> Read_int()
+        {
+            var check = new List<bool>();
+            var input = Console.ReadLine().Split(' ').Select(x => {
+                var temp = 0;
+                check.Add(int.TryParse(x, out temp));
+                return temp;
+            }).ToList();
+            if (check.Any(x => x == false)) return null;
+            return input;
+        }
+    }
+}
